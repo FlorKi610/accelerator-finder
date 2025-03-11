f0f863dbb70e60c8f64ae8df59ff6b965e803e41
Florian Kitterer <fkitterer@microsoft.com>
2025-03-11 18:02:31 +0100

    Updated application files

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,42 +1,56 @@
 import React from 'react';
-import { Box, Text, Link, HStack, Divider } from '@chakra-ui/react';
+import { Box, Text, Link, HStack, Divider, Icon, VStack } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 export const Footer = () => {
   return (
     <Box as="footer" py={12} mt={16}>
       <Divider mb={8} borderColor="gray.200" />
-      <HStack 
-        spacing={4} 
-        justify="center"
-        color="gray.500"
-        fontSize="sm"
-      >
-        <Text>
-          Created by{' '}
+      <VStack spacing={3}>
+        <HStack 
+          spacing={4} 
+          justify="center"
+          color="gray.500"
+          fontSize="sm"
+        >
+          <Text>
+            Created by{' '}
+            <Link 
+              href="mailto:florian.kitterer@microsoft.com" 
+              color="brand.500"
+              fontWeight="medium"
+              _hover={{
+                color: 'brand.600',
+                textDecoration: 'none'
+              }}
+            >
+              Florian Kitterer
+            </Link>
+          </Text>
+          <Box as="span">•</Box>
           <Link 
-            href="mailto:florian.kitterer@microsoft.com" 
-            color="brand.500"
-            fontWeight="medium"
+            href="https://github.com/floriankitterer/acc-find" 
+            color="gray.500"
             _hover={{
-              color: 'brand.600',
+              color: 'brand.500',
               textDecoration: 'none'
             }}
           >
-            Florian Kitterer
+            GitHub
+          </Link>
+        </HStack>
+        <Text fontSize="xs" color="gray.400">
+          Powered by{' '}
+          <Link 
+            href="https://azure.microsoft.com/services/app-service/static/" 
+            isExternal
+            color="blue.400"
+            _hover={{ color: 'blue.500' }}
+          >
+            Azure Static Web Apps <Icon as={ExternalLinkIcon} mx="1px" boxSize="10px" />
           </Link>
         </Text>
-        <Box as="span">•</Box>
-        <Link 
-          href="https://github.com/Azure/azure-openai-landing-zone" 
-          color="gray.500"
-          _hover={{
-            color: 'brand.500',
-            textDecoration: 'none'
-          }}
-        >
-          GitHub
-        </Link>
-      </HStack>
+      </VStack>
     </Box>
   );
 };
